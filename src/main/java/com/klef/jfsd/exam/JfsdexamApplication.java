96c20bc454a1f144133361ce8985b5d114b0c389
upendramola@gmail.com <upendramola@gmail.com>
2024-12-07 13:08:26 +0530

    First commit

---
+++ src/main/java/com/klef/jfsd/exam/JfsdexamApplication.java
@@ -0,0 +1,13 @@
+package com.klef.jfsd.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfsdexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfsdexamApplication.class, args);
+	}
+
+}
